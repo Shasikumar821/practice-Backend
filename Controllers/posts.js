c47d627b7ecfbb676ec22da893b626c79b7b0628
shasi kumar <shasikumar@varnarc.com>
2026-04-02 11:39:14 +0530

    New CRUD oprations

--- Controllers/posts.js
+++ Controllers/posts.js
@@ -13,3 +13,27 @@ exports.getPosts = async (req, res) => {
         res.status(500).json({ error: "Server Error"});
     }
 }
+
+exports.updatePost = async (req, res) => {
+    try {
+        const post = await Post.findOneAndUpdate({ _id: req.params.id, author: req.user.id }, req.body, { new : true });
+        if(!post){
+            return res.status(404).json({ error: "Post not found or you are not the author" });
+        }
+        res.json({ post });
+    }catch (err) {
+        res.status(500).json({ error: "Server Error"});
+    }
+}
+
+exports.deletePost = async (req, res) => {
+    try {
+        const post = await Post.findOneAndDelete({ _id: req.params.id, author: req.user.id }, req.body, { new : true });
+        if(!post){
+            return res.status(404).json({ error: "Post not found or you are not the author" });
+        }
+        res.json({ message: "Post deleted successfully" });
+    }catch (err) {
+        res.status(500).json({ error: "Server Error"});
+    }
+}
--- routers/appDataRoutes.js
+++ routers/appDataRoutes.js
@@ -6,5 +6,7 @@ const { getPosts } = require("../Controllers/posts")
 
 router.post("/posts", createPost)
 router.get("/posts", getPosts)
+router.put("/posts/:id", require("../Controllers/posts").updatePost)
+router.delete("/posts/:id", require("../Controllers/posts").deletePost)
 
 module.exports = router
